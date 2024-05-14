ef496886f7107e8a214636e85571dca6dc003576
LainVegaDamEEP <145350940+LainVegaDamEEP@users.noreply.github.com>
2024-05-14 09:26:34 +0200

    HE BORRADO TODO

--- Kotlin/app/src/main/java/com/example/teetech/TShirt.kt
+++
@@ -1,9 +0,0 @@
-package com.example.teetech
-
-import com.google.gson.annotations.SerializedName
-
-data class TShirt(
-    @SerializedName("id") val id: Long,
-    @SerializedName("name") val name: String,
-    @SerializedName("size") val size: String
-)
